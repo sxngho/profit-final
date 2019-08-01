5e586ececda5532cda524b0ecda88dea6c8bb770
Park Sangho <sxngho@naver.com>
2019-08-01 17:11:10 +0900

    계약서 to pdf

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { VueEditor } from "vue2-editor";
 import ToggleButton from 'vue-js-toggle-button'
 import Notifications from 'vue-notification'
 import VueLoading from 'vuejs-loading-plugin'
+import VueHtml2Canvas from 'vue-html2canvas';
 
 import axios from 'axios'
 import store from './store'
@@ -17,6 +18,7 @@ import 'vuetify/dist/vuetify.min.css'
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
+Vue.use(VueHtml2Canvas);
 Vue.use(VueLoading)
 Vue.use(Notifications)
 Vue.use(ToggleButton)
