a764924c3dbcfbc545d39b1bddfe4c4d9bec8224
Kim-Seul-Gi <rlatmf2147@naver.com>
2019-08-02 10:06:25 +0900

    현재 창에서도 신고 가능

--- src/components/Project/Project.vue
+++ src/components/Project/Project.vue
@@ -12,16 +12,62 @@
          </v-flex>
        </v-toolbar-title>
 
+       <v-btn text color="yellow" @click="submitObjection()" v-if="isMineCheck() && project.state > 0">
+         이의제기
+       </v-btn>
+
       <v-spacer/>
       <v-btn text icon color="pink">
         <i id="likecheck" class="far fa-heart fa-2x" @click="like_project()"></i>
         {{likeit.length}}
         <!-- 이미 좋아요 눌렀다면 다른 fa 를 보여주는 것도 좋겠다. -->
       </v-btn>
+
       <v-btn text icon color="yellow">
         <i class="fa fa-star fa-2x"></i>
       </v-btn>
+
     </v-layout>
+
+    <template>
+    <v-layout justify-center d-inline>
+      <v-dialog v-model="sirendialog" max-width="290">
+        <template v-slot:activator="{ on }">
+          <v-btn text color="primary" dark v-on="on"><i class="fa fa-trash fa-2x"></i></v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="headline">
+            <span class="headline">2신고하기</span>
+          </v-card-title>
+          <v-card-text>
+            <v-layout wrap>
+              <v-flex xs12>
+                <v-combobox
+                v-model="reportSelect"
+                :items="reportItems"
+                label="신고 사유를 선택해주세요."
+                ></v-combobox>
+              </v-flex>
+              <v-flex xs12 v-if="reportSelect=='기타'">
+                <input type="text" v-model="reportText"/>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field v-model="reportDesc" required @keyup.enter="sirendialog = false, submitReport(reportSelect,reportText,reportDesc)"></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="sirendialog = false">취소</v-btn>
+            <v-btn color="blue darken-1" text @click="sirendialog = false, submitReport(reportSelect,reportText,reportDesc)">신고하기</v-btn>
+            <br />
+          </v-card-actions>
+        </v-card>
+
+      </v-dialog>
+    </v-layout>
+  </template>
 
     <!-- card -->
     <v-layout>
@@ -171,6 +217,25 @@ export default {
     likeit: [],
     tmp_taglist:[],
     real_taglist:[],
+    sirendialog: false,
+    reportSelect: '잘못된 정보가 기입된 프로젝트',
+    reportItems: [
+      '잘못된 정보가 기입된 프로젝트',
+      '비속어 사용',
+      '기타',
+    ],
+    reportText: "",
+    reportDesc: "",
+    reportCommentSelect: '비속어 사용',
+    reportCommentItems: [
+      '비속어 사용',
+      '무분별한 비난',
+      '기타',
+    ],
+    reportCommentText: "",
+    reportCommentDesc: "",
+    Commentdialog: false,
+
   }
   },
   props : {
@@ -194,11 +259,70 @@ export default {
          type,
        })
      },
+     async submitObjection() {
+       var objs = await FirebaseService.SELECT_Objections(this.project_id);
+       if ( objs.length == 0 ) { // 이의제기 신청을 하지 않았던 상태
+         FirebaseService.INSERT_Objection(this.project_id,this.project.projecttitle,this.project.state,"Objection");
+       } else {
+        this.showNotification('foo-css','error','이의제기 신청 오류','이미 이의제기 신청이 접수되어있습니다.')
+       }
+     },
+     isMineCheck() {
+       if ( this.user == this.project.session_id ) {
+         return true;
+       } else {
+         return false;
+       }
+     },
+     submitReport(reportSelect,reportText,reportDesc) {
+       var upperUser = this.$session.get('session_id').toUpperCase();
+       if ( !this.project.reportUserList.includes(upperUser) ) {
+         if ( reportSelect !== "기타" ) {
+           FirebaseService.INSERT_projectReport(reportSelect,reportDesc,this.project_id,this.$session.get('session_id')
+                                               ,this.project.session_id,this.project.projecttitle,this.project.state,"Siren_Project");
+           // FirebaseService.INSERT_alert_siren_project(this.project.session_id, this.project, this.userdata)
+         } else {
+           FirebaseService.INSERT_projectReport(reportText,reportDesc,this.project_id,this.$session.get('session_id')
+                                               ,this.project.session_id,this.project.projecttitle,this.project.state,"Siren_Project");
+           // FirebaseService.INSERT_alert_siren_project(this.project.session_id, this.project, this.userdata)
+         }
+         this.project.reportUserList.push(upperUser);
+         FirebaseService.UPDATE_projectReportUserList(this.project_id,this.project.reportUserList);
+       } else {
+         this.showNotification('foo-css','error','프로젝트 신고 오류','이미 신고한 이력이 있는 프로젝트입니다.')
+       }
+       this.reportSelect = "";
+       this.reportText = "";
+       this.reportDesc = "";
+     },
+     submitCommentReport(reportCommentSelect,reportCommentText,reportCommentDesc, comments, index) {
+       // console.log(this.comments[index].reportUserList)
+       var upperUser = this.$session.get('session_id').toUpperCase();
+       if ( !this.comments[index].reportUserList.includes(upperUser) ) {
+         if ( reportCommentSelect !== "기타" ) {
+           FirebaseService.INSERT_commentReport(reportCommentSelect,reportCommentDesc,this.project_id,this.$session.get('session_id')
+                                               ,this.project.session_id,this.project.projecttitle,this.project.state,index,"Siren_Comment");
+         } else {
+           FirebaseService.INSERT_commentReport(reportCommentText,reportCommentDesc,this.project_id,this.$session.get('session_id')
+                                               ,this.project.session_id,this.project.projecttitle,this.project.state,index,"Siren_Comment");
+         }
+
+         this.comments[index].reportUserList.push(upperUser);
+
+         FirebaseService.UPDATE_commentReportUserList(this.project_id,this.comments);
+       } else {
+         this.showNotification('foo-css','error','댓글 신고 오류','이미 신고한 이력이 있는 댓글입니다.')
+       }
+       this.reportCommentSelect = "";
+       this.reportCommentText = "";
+       this.reportCommentDesc = "";
+     },
     async bindData(){
       this.$loading(true)
       this.project = await FirebaseService.SELECT_ProjectsByPcode(this.project_id);
       this.likeit = this.project.likeit
       this.$loading(false)
+      this.userdata = await FirebaseService.SELECT_Userdata(this.user)
     },
     // seulgi function
     async INSERT_Comment(real_taglist, comment){
@@ -232,6 +356,7 @@ export default {
         Comment : listtext + this.comment,
         like : [],
         unlike : [],
+        state:0,
         };
         this.comments.push(newcommnet)
       } else {
