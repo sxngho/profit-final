43ac9a2df3f98c77f6c1542135838fce7008e44b
Kim-Seul-Gi <rlatmf2147@naver.com>
2019-08-13 11:01:27 +0900

    manager page 진행중입니다.

--- src/components/Manager/Main.vue
+++ src/components/Manager/Main.vue
@@ -12,17 +12,28 @@
     <!-- 여기서부터 슬기가 꾸미는 중 -->
     <v-layout row wrap>
       <v-flex>
-        <div style="margin:10px;">
-          <h1 class="headline">관리자 페이지</h1>
+        <div class="fontjua" style="margin:10px;">
+          <h1 class="fontjua">관리자 페이지</h1>
           <br>
 
-            <h2>Weblog</h2>
+            <h2 style="display:inline-block;">Weblog</h2><br>
+            시간별 방문자 추이, 게시판별 방문자 추이 등을 표시합니다.
+            <br><br>
 
-            <h2>Users</h2>
+            <h2 style="display:inline-block;">Users</h2><br>
+            회원가입한 유저들을 나열합니다.<br>
+            운영자 : 일반 유저를 관리자로 조정 가능하고, 다시 유저로 되돌릴 수 있습니다.<br>
+            관리자 : 일반 유저를 탈퇴처리 할 수 있습니다.
+            <br><br>
+            <h2 style="display:inline-block;">Company</h2><br>
+            회원가입한 기업들을 나열합니다.
+            <br><br>
+            <h2 style="display:inline-block;">Siren</h2><br>
+            유저가 프로젝트 혹은 댓글을 <v-icon small class="fa fa-bell fa-1x" v-on="on" color="#ffa76a"></v-icon>
+            신고 처리하면 Siren 페이지에 관련 내용이 등록됩니다.<br>
+            신고 처리된 내용들을 판단하고 나서 신고 stack을 적용시킬 수 있습니다.<br>
+            신고 stack이 일정한 수준(3)을 넘어서면 별도의 블라인드 처리가 됩니다.
 
-            <h2>Company</h2>
-
-            <h2>Siren</h2>
 
         </div>
     </v-flex>
