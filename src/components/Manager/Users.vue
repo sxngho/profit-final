8be25fe1cca10f47e60a88447e72c49f24a05f5e
hyoya <ghtybndss@gmail.com>
2019-08-13 13:32:03 +0900

    manager - Users page design

--- src/components/Manager/Users.vue
+++ src/components/Manager/Users.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <v-layout row wrap>
-      <v-flex xs12 sm10 offset-sm1>
-        당신의 level은 {{level}}
+    <v-layout row wrap justify-center>
+      <v-flex xs12 sm10>
+        <span class="title"><span class="fontYanolja">당신의 등급 : </span></span>
+        <v-chip v-if="level==0" color="red" small class="subtitle-1 white--text"><span class="fontYanolja "> 운영자 </span></v-chip>
+        <v-chip v-else color="success" small class="subtitle-1 white--text"><span class="fontYanolja "> 관리자</span> </v-chip>
         <v-text-field v-model="input" placeholder="유저의 닉네임으로 검색할 수 있습니다."/>
       </v-flex>
     </v-layout>
@@ -20,21 +22,35 @@
                 <v-avatar>
                   <img :src="user.userImage || 'http://design-ec.com/d/e_others_50/l_e_others_500.png'" />
                 </v-avatar>
-                {{user.nickname}} , level : {{user.level}}
+                {{user.nickname}}
+                <v-chip v-if="user.level===0" color="red" small outlined> 운영자 </v-chip>
+                <v-chip v-if="user.level===1" color="success" small outlined> 관리자 </v-chip>
               </div>
             </v-expansion-panel-header>
 
             <v-expansion-panel-content>
-              회원 관리 :
-              <v-btn v-for="(item, index2) in change_state_0" v-if="user.level===1 && level==0" text outlined @click="change_userlevel(user, index2, item)">
-                {{item}}
-              </v-btn>
-              <v-btn v-for="(item, index2) in change_state_1" v-if="user.level===2 && level==0" text outlined @click="change_userlevel(user, index2, item)">
-                {{item}}
-              </v-btn>
-              <v-btn v-for="(item, index2) in change_state_2" v-if="user.level===2 && level==1" text outlined @click="change_userlevel(user, index2, item)">
-                {{item}}
-              </v-btn>
+              <span v-if="(level==0&&(user.level===1 || user.level===2))||(user.level===2 && level==1)">
+                <v-chip text outlined small>
+                  <span class="subtitle-1">
+                    <span class="fontYanolja">{{leveltag[user.level]}}</span>
+                  </span>
+                </v-chip>
+                &nbsp;&nbsp;&nbsp;
+                <span class="title"><span class="fontYanolja">회원관리</span></span>
+                &nbsp;:&nbsp;
+              </span>
+              <span v-else-if="user.level ===0">
+                <v-chip outlined color="red" small>운영자</v-chip>
+              </span>
+              <v-chip v-for="(item, index2) in change_state_0" v-if="user.level===1 && level==0" text @click="change_userlevel(user, index2, item.val)" style="margin-right:2px; color:white;" :color="item.color" v-html="item.tag">
+                <!-- {{item}} -->
+              </v-chip>
+              <v-chip v-for="(item, index2) in change_state_1" v-if="user.level===2 && level==0" text @click="change_userlevel(user, index2, item.val)" style="margin-right:2px; color:white;" :color="item.color" v-html="item.tag">
+                <!-- {{item}} -->
+              </v-chip>
+              <v-chip v-for="(item, index2) in change_state_2" v-if="user.level===2 && level==1" text @click="change_userlevel(user, index2, item.val)" style="margin-right:2px; color:white;" :color="item.color" v-html="item.tag">
+                <!-- {{item}} -->
+              </v-chip>
             </v-expansion-panel-content>
           </v-expansion-panel>
         </v-expansion-panels>
@@ -54,12 +70,24 @@ export default {
   data() {
     return {
       user:1,
+      leveltag:['운영자','관리자','유저','기업'],
       userlist:[],
       input:"",
       userlevel : '',
-      change_state_0:['유저', '탈퇴'],
-      change_state_1:['관리자', '탈퇴'],
-      change_state_2:['탈퇴'],
+      change_state_0:[
+        { tag:'<i class="fas fa-angle-double-down"></i> &nbsp; <span class="subtitle-1 white--text"><span class="fontYanolja">유저</span></span>', color:"success", val:'유저'},
+        { tag:'<span class="subtitle-1 white--text"><span class="fontYanolja">탈퇴</span></span>', color:"red", val:'탈퇴'}
+        // '<i class="fas fa-angle-double-down"></i> 유저', '탈퇴'
+      ],
+      change_state_1:[
+        { tag:'<i class="fas fa-angle-double-up"></i>&nbsp;<span class="subtitle-1 white--text"><span class="fontYanolja">관리자</span></span>', color:"success", val:'관리자'},
+        { tag:'<span class="subtitle-1 white--text"><span class="fontYanolja">탈퇴</span></span>', color:"red", val:'탈퇴'}
+        // '<i class="fas fa-angle-double-up"></i> 관리자', '탈퇴'
+      ],
+      change_state_2:[
+        { tag:'<span class="subtitle-1 white--text"><span class="fontYanolja">탈퇴</span></span>', color:"red", val:'탈퇴'}
+        // '탈퇴'
+      ],
       level : ''
     }
   },
