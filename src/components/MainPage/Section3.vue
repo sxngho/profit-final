aa5dd1e8b56b3143f4f42e9c04b0fd5157f2deae
Kim-Seul-Gi <rlatmf2147@naver.com>
2019-08-13 16:40:08 +0900

    hot portfolio 정리

--- src/components/MainPage/Section3.vue
+++ src/components/MainPage/Section3.vue
@@ -12,44 +12,69 @@
           <!-- <v-carousel cycle hide-delimiters style="height:100%; margin:50px 0;"> -->
             <v-carousel-item v-for="(item, i) in hotprojects">
 
-              <v-card height="100%" class="black--text" style="background:white; padding:50px;">
+              <v-card height="100%" class="black--text" style="background:white;">
 
-                  <div v-if="i<3" style="position:absolute; left:30px; top:20px;">
+                <!-- 원본 시작 -->
+                  <!-- <div v-if="i<3" style="position:absolute; left:30px; top:20px;">
                     <img src="https://i.imgur.com/ZqeqPI2.png"  width="100px" height="100px" />
                     <span style="position:absolute; top:39px; left:45px;">{{i+1}}</span>
                   </div>
+
+
 
                   <div class="mainProjectTitle" style="display:inline-block;  text-overflow: ellipsis; overflow:hidden;">
                     <h2 style="display:inline">{{hotprojects[i][1].projecttitle}}</h2>
                     {{hotprojects[i][1].session_id}}
                   </div>
 
+
                   <div style="display:inline-block; position:absolute; right:5%;">
-                    <!-- <v-badge color="#E74C3C" overlap> -->
-                      <!-- <template slot="badge">  </template> -->
-                        <!-- 이미 좋아요 눌렀다면 다른 fa 를 보여주는 것도 좋겠다. -->
                         <i class="fa fa-heart fa-2x" style="color:red;">
                           <div style="position:relative; top:-23px; font-size:15px; color:white;">
                             <span style="font-weight:bold;">{{hotprojects[i][1].likeitcount}}</span>
                           </div>
                         </i>
+                  </div> -->
+                  <!-- 원본 끝 -->
 
-                    <!-- </v-badge> -->
-                  </div>
+                  <!-- 실험 시작 -->
+                  <v-layout row wrap style="margin:0px;">
+
+                    <v-flex xs0 sm2>
+                      <div v-if="i<3" style="position:absolute; top:10px; left:20px;">
+                        <img src="https://i.imgur.com/ZqeqPI2.png" width="100px" height="100px">
+                          <div style="position:absolute; top:39px; left:46px;">
+                            <span>{{i+1}}</span>
+                          </div>
+                        </img>
+                      </div>
+                    </v-flex>
+
+                    <v-flex xs12 sm8>
+                      <div class="mainProjectTitle" style="margin-top:50px;">
+                        <h2 style="display:inline">{{hotprojects[i][1].projecttitle}}</h2>
+                        <p style="margin-top:10px;">{{hotprojects[i][1].session_id}}</p>
+                      </div>
+                    </v-flex>
+
+                    <v-flex xs0 sm2 >
+                      <div style="position:relative; top:20px;">
+                        <i class="fa fa-heart fa-2x" style="color:red;">
+                          <div style="position:relative; top:-23px; font-size:15px; color:white;">
+                            <span style="font-weight:bold;">{{hotprojects[i][1].likeitcount}}</span>
+                          </div>
+                        </i>
+                      </div>
+                    </v-flex>
+
+                </v-layout>
+                  <!-- 실험 끝 -->
 
                 <v-layout row wrap justify-center style="padding:20px; width:100%; height:90%;">
-                  <!-- <v-flex xs12 hidden-sm-and-up>
-                    <v-avatar size="130">
-                      <img src="https://i.imgur.com/8tFN1Zj.jpg" alt="gg"/>
-                    </v-avatar>
-                  </v-flex> -->
 
                   <v-flex sm5 hidden-xs-only style="overflow:hidden; padding:20px 0;">
 
                       <img :src="hotprojects[i][1].projectimage || 'https://i.imgur.com/8tFN1Zj.jpg'" alt="gg" style="width:250px; height:250px;"/>
-
-                    <!-- <img :src="company.company_logo || 'http://design-ec.com/d/e_others_50/l_e_others_500.png'" style="width:150px; height:90px" /> -->
-
                   </v-flex>
                   <v-flex xs12 offset-sm1 sm5 style="padding:20px 0;">
                     <span class="grey--text caption txtBox2">{{hotprojects[i][1].projectdescription}}</span>
@@ -60,19 +85,18 @@
                       </v-chip>
                       <br/><br/>
                       <!-- <span>상세내용</span><br/> -->
-                      <p v-html="hotprojects[i][1].projectcontent" class="txtBox2"></p>
+                      <p v-html="hotprojects[i][1].projectcontent" class="txtBox3"></p>
                     </div>
 
 
                   </v-flex>
 
-
+                  <div class="text-center;" style="margin-top:-10px; position:absolute; bottom:10px;">
+                    <v-btn color="primary" @click="gogo(hotprojects[i][0])">보러가기</v-btn>
+                  </div>
 
                 </v-layout>
 
-                <div class="text-center;">
-                  <v-btn color="primary" @click="gogo(hotprojects[i][0])">보러가기</v-btn>
-                </div>
 
               </v-card>
 
