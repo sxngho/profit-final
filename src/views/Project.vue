c8242cd084944ff13b167185444335370d2d40ee
Park Sangho <sxngho@naver.com>
2019-08-14 12:13:12 +0900

    신고기능 해결

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -320,10 +320,10 @@ export default {
         }
         this.project.reportUserList.push(upperUser);
         FirebaseService.UPDATE_projectReportUserList(this.project_id,this.project.reportUserList);
+        this.$swal("프로젝트 신고 성공!", "성공적으로 신고내용이 접수되었습니다.", "success");
       } else {
-        this.showNotification('foo-css','error','프로젝트 신고 오류','이미 신고한 이력이 있는 프로젝트입니다.')
+        this.$swal("프로젝트 신고 오류!", "이미 신고한 이력이 있는 프로젝트입니다.", "error");
       }
-      this.reportSelect = "";
       this.reportText = "";
       this.reportDesc = "";
     },
@@ -342,10 +342,10 @@ export default {
         this.comments[index].reportUserList.push(upperUser);
 
         FirebaseService.UPDATE_commentReportUserList(this.project_id,this.comments);
+        this.$swal("댓글 신고 성공!", "성공적으로 신고내용이 접수되었습니다.", "success");
       } else {
-        this.showNotification('foo-css','error','댓글 신고 오류','이미 신고한 이력이 있는 댓글입니다.')
+        this.$swal("댓글 신고 오류!", "이미 신고한 이력이 있는 댓글입니다.", "error");
       }
-      this.reportCommentSelect = "";
       this.reportCommentText = "";
       this.reportCommentDesc = "";
     },
@@ -403,6 +403,7 @@ export default {
           like : [],
           unlike : [],
           state:0,
+          reportUserList:[],
           date: (date.getFullYear()-2000) + "." + (date.getMonth()+1) + "."  + date.getDate() + "." + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
         };
         this.comments.push(newcommnet)
